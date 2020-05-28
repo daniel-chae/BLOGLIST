61f68845c397dbda6093034a0b90df0f405928b3
daniel-chae <daniel.chae.a@gmail.com>
2020-05-28 10:36:47 +0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,49 @@
+// app.js creates an express app, connect to mongoDB, and set middlewares and routers for the app
+
+// require external modules
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+// require internal modules
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const blogsRouter = require('./controllers/blogs');
+const middleware = require('./utils/middleware');
+const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
+
+// create an express app
+const app = express();
+
+// connect to mongoDB
+mongoose
+  .connect(config.mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    logger.info('connected to database');
+  })
+  .catch((err) => logger.error(err.message));
+
+// set middlewares
+app.use(cors());
+app.use(express.json());
+app.use(middleware.tokenExtractor);
+
+// set routers
+app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
+app.use('/login', loginRouter);
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing');
+  app.use('/api/testing/', testingRouter);
+}
+
+// set error handler
+app.use(middleware.errorHandler);
+
+module.exports = app;
---
+++ controllers/blogs.js
@@ -0,0 +1,167 @@
+// blog.js creates an express router and set routes with route controllers
+
+// require external modules
+const router = require('express').Router();
+require('express-async-errors');
+const jwt = require('jsonwebtoken');
+
+// require internal modules
+const Blog = require('../models/blog');
+const User = require('../models/user');
+
+// Set route handlers
+
+// Create a blog
+router.post('/', async (request, response) => {
+  const body = request.body;
+
+  const decodedToken =
+    request.token === undefined
+      ? false
+      : jwt.verify(request.token, process.env.SECRET);
+
+  if (!(request.token && decodedToken)) {
+    return response.status(401).json({ error: 'token missing or invalid' });
+  }
+
+  const user = await User.findById(decodedToken.id);
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user._id,
+  });
+
+  const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
+
+  const foundBlog = await Blog.findById(savedBlog._id).populate('user', {
+    username: 1,
+    name: 1,
+  });
+  return response.status(201).json(foundBlog);
+});
+
+// Read blogs
+router.get('/', async (request, response) => {
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
+  return response.json(blogs);
+});
+
+// Read a blog
+router.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id).populate('user', {
+    username: 1,
+    name: 1,
+  });
+  return response.json(blog);
+});
+
+// Update a blog (PUT METHOD)
+router.put('/:id', async (request, response) => {
+  const decodedToken =
+    request.token === undefined
+      ? false
+      : jwt.verify(request.token, process.env.SECRET);
+
+  if (!(request.token && decodedToken)) {
+    return response.status(401).json({ error: 'token missing or invalid' });
+  }
+
+  const blogToUpdate = await Blog.findById(request.params.id);
+
+  if (!blogToUpdate) {
+    return response.status(204).json({ error: 'No matching blog found' });
+  }
+
+  const user = await User.findById(decodedToken.id);
+
+  request.body.user = blogToUpdate.user;
+  request.body.__v = blogToUpdate.__v;
+  request.body.comments = blogToUpdate.comments;
+
+  const updatedBlog = await Blog.findOneAndReplace(
+    { _id: request.params.id },
+    request.body,
+    {
+      new: true,
+      runValidators: true,
+    }
+  ).populate('user', {
+    username: 1,
+    name: 1,
+  });
+
+  response.status(200).json(updatedBlog);
+});
+
+// Update a blog (PATCH METHOD)
+router.post('/:id/comments', async (request, response) => {
+  const decodedToken =
+    request.token === undefined
+      ? false
+      : jwt.verify(request.token, process.env.SECRET);
+
+  if (!(request.token && decodedToken)) {
+    return response.status(401).json({ error: 'token missing or invalid' });
+  }
+
+  const blogToUpdate = await Blog.findById(request.params.id);
+
+  if (!blogToUpdate) {
+    return response.status(204).json({ error: 'No matching blog found' });
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    request.params.id,
+    { comments: [...blogToUpdate.comments, request.body.comment] },
+    {
+      new: true,
+      runValidators: true,
+    }
+  ).populate('user', {
+    username: 1,
+    name: 1,
+  });
+
+  response.status(200).json(updatedBlog);
+});
+
+// Delete a blog
+router.delete('/:id', async (request, response) => {
+  const decodedToken =
+    request.token === undefined
+      ? false
+      : jwt.verify(request.token, process.env.SECRET);
+
+  if (!(request.token && decodedToken)) {
+    return response.status(401).json({ error: 'token missing or invalid' });
+  }
+
+  const blogToDelete = await Blog.findById(request.params.id);
+
+  if (!blogToDelete) {
+    return response.status(204).json({ error: 'No matching blog found' });
+  }
+
+  if (blogToDelete.user.toString() !== decodedToken.id) {
+    return response
+      .status(401)
+      .json({ error: 'The owner of the token is not the owner of the blog' });
+  }
+
+  const user = await User.findById(decodedToken.id);
+
+  await Blog.findByIdAndDelete(request.params.id);
+  user.blogs = user.blogs.filter(
+    (blogId) => blogId.toString() !== request.params.id
+  );
+
+  await user.save();
+  response.status(204).end();
+});
+
+module.exports = router;
